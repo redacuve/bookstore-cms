938a72d8a409ec7153afb7d4c9c1f39cca22a9b6
Rey David Cuevas <cuevas.vela@gmail.com>
2020-08-06 22:03:37 -0500

    change BooksForm from function to class

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -1,20 +1,39 @@
 import React from 'react';
 
-const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
-// eslint-disable-next-line no-unused-vars
-function BooksForm(props) {
-  return (
-    <form>
-      <label htmlFor="book-title">
-        Book Title
-        <input type="text" value="" id="book-title" />
-      </label>
-      <select name="categories" id="categories">
-        { categories.map(c => <option key={c} value={c}>{c}</option>)}
-      </select>
-      <button type="button">Save Book</button>
-    </form>
-  );
+const categories = [
+  'Action',
+  'Biography',
+  'History',
+  'Horror',
+  'Kids',
+  'Learning',
+  'Sci-Fi',
+];
+
+class BooksForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <form>
+        <label htmlFor="book-title">
+          Book Title
+          <input type="text" value="" id="book-title" />
+        </label>
+        <select name="categories" id="categories">
+          {categories.map(c => (
+            <option key={c} value={c}>
+              {c}
+            </option>
+          ))}
+        </select>
+        <button type="button">Save Book</button>
+      </form>
+    );
+  }
 }
 
 export default BooksForm;
