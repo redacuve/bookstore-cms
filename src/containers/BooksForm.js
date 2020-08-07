328eb3db4d71510d5399c75a47a87b1394bd1d4d
Rey David Cuevas <cuevas.vela@gmail.com>
2020-08-07 14:45:16 -0500

    added robotoSlab font

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -56,7 +56,7 @@ class BooksForm extends React.Component {
               </option>
             ))}
           </select>
-          <button type="submit">Save Book</button>
+          <button className="add-book" type="submit">Save Book</button>
         </form>
       </div>
     );
